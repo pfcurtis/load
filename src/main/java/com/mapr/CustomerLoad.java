55cef563f16a9e24afb88a8d4e0bf6353a7e9bc1
Ted Dunning <ted.dunning@gmail.com>
2012-07-15 01:35:24 -0700

    Started CustomerLoad program.  Added real traces.

---
+++ src/main/java/com/mapr/CustomerLoad.java
@@ -0,0 +1,25 @@
+package com.mapr;
+
+/**
+ * Simulates actual customer load.  This uses either of two traces that are
+ * resources in the program and runs a load that consists of a mixture of a
+ *
+ * 1) database log which is sequentially written and then truncated and rewritten.
+ *
+ * 2) database file that is written with uneven random access
+ *
+ * 3) database reads that are uniform random
+ *
+ * 4) log writes that are sequential
+ *
+ * 5) log reads that read recently written blocks from (4)
+ *
+ * 6) every half hour, a substantial read occurs of about 6000 blocks over a
+ * period of 5 seconds or so.
+ *
+ * Parameters are the proportion of writes that make up the total.  The reads
+ * are determined by this because the reads in (5) are determined by the volume
+ * of writes in (4).
+ */
+public class CustomerLoad {
+}
