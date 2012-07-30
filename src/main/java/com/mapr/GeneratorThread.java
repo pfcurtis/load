ce77dbe38fc2920503098e2854bcaea680a1e9f9
Paul Curtis <pcurtis@maprtech.com>
2012-07-30 15:07:50 -0400

    Initial CustomerLoad class. Starts <n> threads of RandomFile generator

---
+++ src/main/java/com/mapr/GeneratorThread.java
@@ -0,0 +1,40 @@
+package com.mapr;
+
+import com.mapr.load.*;
+import java.io.File;
+import java.io.IOException;
+import java.net.InetAddress;
+
+public class GeneratorThread extends Thread {
+   private String[] args;
+
+   public GeneratorThread(String[] a) {
+      this.args = a;
+   }
+
+   public void run() {
+      File file = null;
+
+      Generator g = new Generator();
+      g.setBlockSize(4096);
+
+      try {
+         String hostname = java.net.InetAddress.getLocalHost().getHostName();
+         file = new File(hostname + "-" + this.getId());
+         file.deleteOnExit();
+
+         final Filer actor = RandomFiler.create(file, 1000000, 1, 1);
+         for (String trace : args) {
+            actor.reset(actor.currentTime());
+            System.out.println("Adding trace "+trace);
+            g.addTrace(Generator.readTraceFile(new File(trace)));
+            g.generate(actor);
+         }
+      } catch (Exception e) {
+         return;
+      }
+
+   }
+}
+
+
